4cdbddd127ed481afee3dff8b9c4a97c4e1097c5
marina <marina.gherman@icloud.com>
2022-07-13 15:32:20 +0200

    add header

--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import {Col, Row} from "antd";
+import logo from './images/logo.png'
+import s from './Header.module.scss'
+
 
 const Header = () => {
     return (
-        <div>
+        <div className={s.header}>
+        <Row>
+            <Col span={1}>
+                <div>
+                    <img src={logo} alt=""/>
+                </div>
+            </Col>
+            <Col span={10}>
+               <div></div>
+            </Col>
+            <Col span={1}>
+                <div></div>
+            </Col>
+        </Row>
             
         </div>
     );
